a9de3f60afd720289a1b74390016d055afa0cc3b
Kevin Nielsen <knielsen24@gmail.com>
2022-04-25 16:27:59 -0700

    commit tree b3025e8ab4a58998ec55e9b5719ee5356d855760

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    if (x >= 42) {
+        return x - 42;
+    }
+    else {
+        return 42 - x;
+    }
+}
+
+console.log(distanceFromHqInBlocks(43));
+
+function distanceFromHqInFeet(x) {
+    return distanceFromHqInBlocks(x) * 264;
+}
+
+console.log(distanceFromHqInFeet(43));
+
+
+function distanceTravelledInFeet(x, y) {
+    if (x >= y) {
+        return (x - y) * 264;
+    }
+    else {
+        return (y - x) * 264;
+    }
+}
+
+console.log(distanceTravelledInFeet(50, 42));
+
+function calculatesFarePrice(x, y) {
+    if (distanceTravelledInFeet(x, y) <= 400) {
+        return 0;
+    }
+    else if (distanceTravelledInFeet(x, y) > 400 && distanceTravelledInFeet(x, y) <= 2000) {
+        return (distanceTravelledInFeet(x, y) - 400) * .02;
+    }
+    else if (distanceTravelledInFeet(x, y) > 2000 && distanceTravelledInFeet(x, y) < 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+
+}
